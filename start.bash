8b26b96aaaa2f441c0add6a77a4ce0dfe90186f0
KadotaMasayuki <44630941+KadotaMasayuki@users.noreply.github.com>
2024-01-14 17:57:21 +0900

    Update start.bash

--- start.bash
+++ start.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name jupyter-container --rm --detach --publish 8889:8888 --mount type=bind,src=$PWD,dst=/home/jupyter/src jupyter
+docker run --name jupyter-container --rm --detach --publish 8889:8888 --mount type=bind,src=$PWD,dst=/jupyter --workdir /jupyter jupyter
